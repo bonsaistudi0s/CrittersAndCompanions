586581fa1ba0e908a9f848025e8b928b906de2d8
EterDelta <simplysilvercube@gmail.com>
2022-07-26 01:41:21 -0500

    Mixins cleanup

--- src/main/java/com/github/eterdelta/crittersandcompanions/mixin/ClientPacketListenerMixin.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/mixin/ClientPacketListenerMixin.java
@@ -7,9 +7,7 @@ import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.protocol.game.ClientboundAddMobPacket;
 import net.minecraft.world.entity.LivingEntity;
 import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,10 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ClientPacketListener.class)
 public class ClientPacketListenerMixin {
-
-    @Final
-    @Shadow
-    private Minecraft minecraft;
 
     @Inject(at = @At(value = "JUMP", opcode = Opcodes.IFEQ, ordinal = 0), method = "handleAddMob(Lnet/minecraft/network/protocol/game/ClientboundAddMobPacket;)V", locals = LocalCapture.CAPTURE_FAILSOFT)
     private void handleAddMob(ClientboundAddMobPacket packet, CallbackInfo callback, LivingEntity packetEntity) {
