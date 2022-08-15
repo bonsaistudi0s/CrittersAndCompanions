5038025338d8c0b1f67711ff4817a5bc78c4fdd8
EterDelta <simplysilvercube@gmail.com>
2022-08-15 03:52:01 -0500

    Fix dragonfly spawn rule height

--- src/main/java/com/github/eterdelta/crittersandcompanions/entity/DragonflyEntity.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/entity/DragonflyEntity.java
@@ -41,7 +41,7 @@ public class DragonflyEntity extends PathfinderMob implements IAnimatable {
     }
 
     public static boolean checkDragonflySpawnRules(EntityType<DragonflyEntity> entityType, LevelAccessor levelAccessor, MobSpawnType spawnType, BlockPos blockPos, Random random) {
-        return levelAccessor.getBlockState(blockPos).isAir() && levelAccessor.getRawBrightness(blockPos, 0) > 8;
+        return blockPos.getY() > 63 && levelAccessor.getBlockState(blockPos).isAir() && levelAccessor.getRawBrightness(blockPos, 0) > 8;
     }
 
     @Override
