ebe27f3291c7a49bc74fc69d5be1e74bd8aff694
EterDelta <simplysilvercube@gmail.com>
2022-11-27 23:16:22 -0500

    Don't cancel the callback

--- src/main/java/com/github/eterdelta/crittersandcompanions/mixin/ClientPacketListenerMixin.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/mixin/ClientPacketListenerMixin.java
@@ -4,25 +4,19 @@ import com.github.eterdelta.crittersandcompanions.client.sound.DragonflySoundIns
 import com.github.eterdelta.crittersandcompanions.entity.DragonflyEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ClientPacketListener.class)
 public class ClientPacketListenerMixin {
 
     @Inject(at = @At("HEAD"), method = "postAddEntitySoundInstance", cancellable = true)
-    private void handleAddMob(Entity flag, CallbackInfo ci) {
-        if (flag instanceof DragonflyEntity dragonflyEntity) {
+    private void handleAddMob(Entity packetEntity, CallbackInfo callback) {
+        if (packetEntity instanceof DragonflyEntity dragonflyEntity) {
             Minecraft.getInstance().getSoundManager().queueTickingSound(new DragonflySoundInstance(dragonflyEntity));
-
-            ci.cancel();
         }
     }
 }
