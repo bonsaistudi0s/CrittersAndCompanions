8f871c98d79f4aca896ea09dcb70f062910b6939
EterDelta <simplysilvercube@gmail.com>
2023-01-15 20:17:49 -0500

    Fix LivingEntityMixin injection point

--- src/main/java/com/github/eterdelta/crittersandcompanions/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/mixin/LivingEntityMixin.java
@@ -18,7 +18,7 @@ public abstract class LivingEntityMixin extends Entity {
         super(entityType, level);
     }
 
-    @ModifyVariable(at = @At(value = "STORE", ordinal = 0), method = "travel(Lnet/minecraft/world/phys/Vec3;)V", ordinal = 1)
+    @ModifyVariable(at = @At(value = "LOAD", ordinal = 3), method = "travel(Lnet/minecraft/world/phys/Vec3;)V", ordinal = 1)
     private float modifySwimSpeed(float swimSpeed) {
         if (((Entity) this) instanceof Player player) {
             Inventory inventory = player.getInventory();
