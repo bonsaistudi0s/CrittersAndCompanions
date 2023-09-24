c25a45a990647d4ededf6dadee116adf59323c27
EterDelta <67644822+EterDelta@users.noreply.github.com>
2023-09-24 14:09:01 -0500

    Increase dragonfly armors health buff

--- src/main/java/com/github/eterdelta/crittersandcompanions/registry/CACItems.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/registry/CACItems.java
@@ -33,9 +33,9 @@ public class CACItems {
     public static final RegistryObject<Item> KOI_FISH_BUCKET = ITEMS.register("koi_fish_bucket", () -> new MobBucketItem(CACEntities.KOI_FISH, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH, (new Item.Properties()).stacksTo(1).tab(CrittersAndCompanions.CREATIVE_TAB)));
     public static final RegistryObject<Item> SEA_BUNNY_BUCKET = ITEMS.register("sea_bunny_bucket", () -> new MobBucketItem(CACEntities.SEA_BUNNY, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH, (new Item.Properties()).stacksTo(1).tab(CrittersAndCompanions.CREATIVE_TAB)));
 
-    public static final RegistryObject<Item> DIAMOND_DRAGONFLY_ARMOR = ITEMS.register("diamond_dragonfly_armor", () -> new DragonflyArmorItem(12, "diamond", (new Item.Properties()).stacksTo(1).tab(CrittersAndCompanions.CREATIVE_TAB)));
-    public static final RegistryObject<Item> GOLD_DRAGONFLY_ARMOR = ITEMS.register("gold_dragonfly_armor", () -> new DragonflyArmorItem(6, "gold", (new Item.Properties()).stacksTo(1).tab(CrittersAndCompanions.CREATIVE_TAB)));
-    public static final RegistryObject<Item> IRON_DRAGONFLY_ARMOR = ITEMS.register("iron_dragonfly_armor", () -> new DragonflyArmorItem(3, "iron", (new Item.Properties()).stacksTo(1).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> DIAMOND_DRAGONFLY_ARMOR = ITEMS.register("diamond_dragonfly_armor", () -> new DragonflyArmorItem(16, "diamond", (new Item.Properties()).stacksTo(1).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> GOLD_DRAGONFLY_ARMOR = ITEMS.register("gold_dragonfly_armor", () -> new DragonflyArmorItem(8, "gold", (new Item.Properties()).stacksTo(1).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> IRON_DRAGONFLY_ARMOR = ITEMS.register("iron_dragonfly_armor", () -> new DragonflyArmorItem(4, "iron", (new Item.Properties()).stacksTo(1).tab(CrittersAndCompanions.CREATIVE_TAB)));
 
     public static final RegistryObject<Item> DRAGONFLY_SPAWN_EGG = ITEMS.register("dragonfly_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.DRAGONFLY, 0x08EECF, 0xD3FF96, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
     public static final RegistryObject<Item> FERRET_SPAWN_EGG = ITEMS.register("ferret_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.FERRET, 0xC5AC88, 0x37212D, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
