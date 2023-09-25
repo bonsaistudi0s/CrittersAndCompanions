f71a6df4ee5913ce0a91f85871ff6e92e075f1b7
EterDelta <67644822+EterDelta@users.noreply.github.com>
2023-09-24 23:02:45 -0500

    Animate Jumping Spider head_rotation bone

--- src/main/java/com/github/eterdelta/crittersandcompanions/client/model/geo/JumpingSpiderModel.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/client/model/geo/JumpingSpiderModel.java
@@ -2,8 +2,12 @@ package com.github.eterdelta.crittersandcompanions.client.model.geo;
 
 import com.github.eterdelta.crittersandcompanions.CrittersAndCompanions;
 import com.github.eterdelta.crittersandcompanions.entity.JumpingSpiderEntity;
+import com.github.eterdelta.crittersandcompanions.entity.RedPandaEntity;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class JumpingSpiderModel extends AnimatedGeoModel<JumpingSpiderEntity> {
     private static final ResourceLocation MODEL = new ResourceLocation(CrittersAndCompanions.MODID, "geo/jumping_spider.geo.json");
@@ -23,5 +27,16 @@ public class JumpingSpiderModel extends AnimatedGeoModel<JumpingSpiderEntity> {
     @Override
     public ResourceLocation getAnimationResource(JumpingSpiderEntity animatable) {
         return ANIMATION;
+    }
+
+    @Override
+    public void setCustomAnimations(JumpingSpiderEntity entity, int uniqueID, AnimationEvent customPredicate) {
+        super.setCustomAnimations(entity, uniqueID, customPredicate);
+        IBone headBone = this.getAnimationProcessor().getBone("head_rotation");
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+
+        headBone.setRotationX(extraData.headPitch * ((float) Math.PI / 180.0F));
+        headBone.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 180.0F));
+        entity.getFactory().getOrCreateAnimationData(uniqueID).setResetSpeedInTicks(0);
     }
 }
