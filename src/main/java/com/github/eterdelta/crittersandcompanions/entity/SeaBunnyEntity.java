b9b87ce322ae41145c45e3cb3ab640dd891dc503
Cephelo <79186646+Cephelo@users.noreply.github.com>
2024-08-20 16:11:32 -0500

    Set SeaBunny MobType to Water

--- src/main/java/com/github/eterdelta/crittersandcompanions/entity/SeaBunnyEntity.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/entity/SeaBunnyEntity.java
@@ -19,6 +19,7 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -83,6 +84,10 @@ public class SeaBunnyEntity extends WaterAnimal implements Bucketable, GeoEntity
         this.entityData.define(CLIMBING, false);
         this.entityData.define(VARIANT, 0);
         this.entityData.define(FROM_BUCKET, false);
+    }
+
+    public MobType getMobType() {
+        return MobType.WATER;
     }
 
     @Override
