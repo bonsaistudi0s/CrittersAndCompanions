485b0a9ed7a8394bffbdba391f856c2237b4b292
Niklas Widmann <nik@somethingcatchy.net>
2025-06-28 23:42:59 +0200

    run spotless

--- common/src/main/java/com/github/eterdelta/crittersandcompanions/block/SeaBunnySlimeBlock.java
+++ common/src/main/java/com/github/eterdelta/crittersandcompanions/block/SeaBunnySlimeBlock.java
@@ -8,7 +8,6 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.HalfTransparentBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
