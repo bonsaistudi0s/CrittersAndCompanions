57e679382edaa18ba5f33ee97d8777540b3e3368
Niklas Widmann <nik@somethingcatchy.net>
2025-07-22 18:20:10 +0200

    jumping spiders should not attack other tames of their owner

--- common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/JumpingSpiderEntity.java
+++ common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/JumpingSpiderEntity.java
@@ -190,4 +190,12 @@ public class JumpingSpiderEntity extends TamableAnimal implements GeoEntity {
         return effect.getEffect() != MobEffects.POISON && super.canBeAffected(effect);
     }
 
+    @Override
+    public boolean wantsToAttack(LivingEntity target, LivingEntity owner) {
+        if (target instanceof TamableAnimal tamable) {
+            return !tamable.isTame() || tamable.getOwner() != owner;
+        }
+
+        return super.wantsToAttack(target, owner);
+    }
 }
