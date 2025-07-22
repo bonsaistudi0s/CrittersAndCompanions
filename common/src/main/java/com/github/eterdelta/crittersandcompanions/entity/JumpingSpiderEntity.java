be201a00f2b740a860979acf0871393dc6278991
Niklas Widmann <nik@somethingcatchy.net>
2025-07-22 18:11:14 +0200

    jumping spiders should be immune to poison

--- common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/JumpingSpiderEntity.java
+++ common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/JumpingSpiderEntity.java
@@ -7,6 +7,8 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -182,4 +184,10 @@ public class JumpingSpiderEntity extends TamableAnimal implements GeoEntity {
             super.tick();
         }
     }
+
+    @Override
+    public boolean canBeAffected(MobEffectInstance effect) {
+        return effect.getEffect() != MobEffects.POISON && super.canBeAffected(effect);
+    }
+
 }
