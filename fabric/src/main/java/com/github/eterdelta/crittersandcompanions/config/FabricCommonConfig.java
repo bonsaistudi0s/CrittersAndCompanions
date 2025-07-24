BoyAxl/CrittersAndCompanions-Fabric-Port

add config system

fabric/src/main/java/com/github/eterdelta/crittersandcompanions/config/FabricCommonConfig.java
@@ -1,0 +1,69 @@
+package com.github.eterdelta.crittersandcompanions.config;
+
+import com.github.eterdelta.crittersandcompanions.CACWorldGen;
+import com.github.eterdelta.crittersandcompanions.registry.CACEntities;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.EntityType;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+public class FabricCommonConfig extends CACCommonConfig {
+
+    private static final Map<ResourceKey<? extends EntityType<?>>, CACWorldGen.SpawnValues> SPAWN_DEFAULT_VALUES = new HashMap<>();
+
+    private final Map<ResourceKey<? extends EntityType<?>>, Supplier<CACWorldGen.SpawnValues>> spawnValues = new HashMap<>();
+
+    private void registerSpawns(ModConfigSpec.Builder builder, ResourceKey<? extends EntityType<?>> key, CACWorldGen.SpawnValues defaultValues) {
+        builder.push(key.location().getPath());
+
+        var weight = builder.defineInRange("weight", defaultValues.weight(), 0, 128);
+        var min = builder.defineInRange("min", defaultValues.min(), 1, 128);
+        var max = builder.defineInRange("max", defaultValues.max(), 1, 128);
+
+        spawnValues.put(key, () -> new CACWorldGen.SpawnValues(weight.getAsInt(), min.get(), max.getAsInt()));
+
+        builder.pop();
+    }
+
+    public CACWorldGen.SpawnValues getSpawnValues(ResourceKey<? extends EntityType<?>> key) {
+        return Objects.requireNonNull(spawnValues.get(key)).get();
+    }
+
+    public FabricCommonConfig(ModConfigSpec.Builder builder) {
+        super(builder);
+
+        builder.push("spawn_rates");
+
+        registerSpawns(builder, CACEntities.LEAF_INSECT.getKey(), new CACWorldGen.SpawnValues(14, 1, 1));
+        registerSpawns(builder, CACEntities.RED_PANDA.getKey(), new CACWorldGen.SpawnValues(8, 1, 2));
+        registerSpawns(builder, CACEntities.JUMPING_SPIDER.getKey(), new CACWorldGen.SpawnValues(2, 1, 1));
+        registerSpawns(builder, CACEntities.LEAF_INSECT.getKey(), new CACWorldGen.SpawnValues(14, 1, 1));
+        registerSpawns(builder, CACEntities.FERRET.getKey(), new CACWorldGen.SpawnValues(3, 2, 3));
+        registerSpawns(builder, CACEntities.JUMPING_SPIDER.getKey(), new CACWorldGen.SpawnValues(2, 1, 1));
+        registerSpawns(builder, CACEntities.JUMPING_SPIDER.getKey(), new CACWorldGen.SpawnValues(2, 1, 1));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(16, 1, 2));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(4, 1, 1));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(16, 1, 2));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(4, 1, 1));
+        registerSpawns(builder, CACEntities.FERRET.getKey(), new CACWorldGen.SpawnValues(4, 2, 3));
+        registerSpawns(builder, CACEntities.FERRET.getKey(), new CACWorldGen.SpawnValues(4, 2, 3));
+        registerSpawns(builder, CACEntities.OTTER.getKey(), new CACWorldGen.SpawnValues(1, 3, 5));
+        registerSpawns(builder, CACEntities.KOI_FISH.getKey(), new CACWorldGen.SpawnValues(4, 2, 5));
+        registerSpawns(builder, CACEntities.DRAGONFLY.getKey(), new CACWorldGen.SpawnValues(7, 1, 1));
+        registerSpawns(builder, CACEntities.SHIMA_ENAGA.getKey(), new CACWorldGen.SpawnValues(3, 2, 3));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(32, 1, 4));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(8, 1, 1));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(16, 1, 4));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(6, 1, 1));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(16, 1, 4));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(6, 1, 1));
+
+        builder.pop();
+
+        CACWorldGen.registerSpawns(this);
+    }
+
+}

neoforge/src/main/java/com/github/eterdelta/crittersandcompanions/CrittersAndCompanionsForge.java
@@ -3,6 +3,7 @@ package com.github.eterdelta.crittersandcompanions;
 import static com.github.eterdelta.crittersandcompanions.CrittersAndCompanions.MODID;
 
 import com.github.eterdelta.crittersandcompanions.handler.PlayerHandler;
+import com.github.eterdelta.crittersandcompanions.platform.ForgeConfigs;
 import com.github.eterdelta.crittersandcompanions.platform.ForgeNetwork;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -12,6 +13,7 @@ import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.world.item.context.UseOnContext;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
@@ -28,9 +30,10 @@ import net.neoforged.neoforge.registries.NeoForgeRegistries;
 @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
 public class CrittersAndCompanionsForge {
 
-    public CrittersAndCompanionsForge(IEventBus modBus) {
+    public CrittersAndCompanionsForge(ModContainer container, IEventBus modBus) {
         CrittersAndCompanions.init();
         ForgeNetwork.register(modBus);
+        ForgeConfigs.register(container);
 
         modBus.addListener((FMLCommonSetupEvent event) -> event.enqueueWork(CrittersAndCompanions::setup));
         modBus.addListener((FMLClientSetupEvent event) -> event.enqueueWork(CrittersAndCompanionsClient::init));

bonsaistudi0s/CrittersAndCompanions

fabric/src/main/java/com/github/eterdelta/crittersandcompanions/config/FabricCommonConfig.java
@@ -1,0 +1,69 @@
+package com.github.eterdelta.crittersandcompanions.config;
+
+import com.github.eterdelta.crittersandcompanions.CACWorldGen;
+import com.github.eterdelta.crittersandcompanions.registry.CACEntities;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class FabricCommonConfig extends CACCommonConfig {
+
+    private static final Map<ResourceKey<? extends EntityType<?>>, CACWorldGen.SpawnValues> SPAWN_DEFAULT_VALUES = new HashMap<>();
+
+    private final Map<ResourceKey<? extends EntityType<?>>, Supplier<CACWorldGen.SpawnValues>> spawnValues = new HashMap<>();
+
+    private void registerSpawns(ForgeConfigSpec.Builder builder, ResourceKey<? extends EntityType<?>> key, CACWorldGen.SpawnValues defaultValues) {
+        builder.push(key.location().getPath());
+
+        var weight = builder.defineInRange("weight", defaultValues.weight(), 0, 128);
+        var min = builder.defineInRange("min", defaultValues.min(), 1, 128);
+        var max = builder.defineInRange("max", defaultValues.max(), 1, 128);
+
+        spawnValues.put(key, () -> new CACWorldGen.SpawnValues(weight.get(), min.get(), max.get()));
+
+        builder.pop();
+    }
+
+    public CACWorldGen.SpawnValues getSpawnValues(ResourceKey<? extends EntityType<?>> key) {
+        return Objects.requireNonNull(spawnValues.get(key)).get();
+    }
+
+    public FabricCommonConfig(ForgeConfigSpec.Builder builder) {
+        super(builder);
+
+        builder.push("spawn_rates");
+
+        registerSpawns(builder, CACEntities.LEAF_INSECT.getKey(), new CACWorldGen.SpawnValues(14, 1, 1));
+        registerSpawns(builder, CACEntities.RED_PANDA.getKey(), new CACWorldGen.SpawnValues(8, 1, 2));
+        registerSpawns(builder, CACEntities.JUMPING_SPIDER.getKey(), new CACWorldGen.SpawnValues(2, 1, 1));
+        registerSpawns(builder, CACEntities.LEAF_INSECT.getKey(), new CACWorldGen.SpawnValues(14, 1, 1));
+        registerSpawns(builder, CACEntities.FERRET.getKey(), new CACWorldGen.SpawnValues(3, 2, 3));
+        registerSpawns(builder, CACEntities.JUMPING_SPIDER.getKey(), new CACWorldGen.SpawnValues(2, 1, 1));
+        registerSpawns(builder, CACEntities.JUMPING_SPIDER.getKey(), new CACWorldGen.SpawnValues(2, 1, 1));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(16, 1, 2));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(4, 1, 1));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(16, 1, 2));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(4, 1, 1));
+        registerSpawns(builder, CACEntities.FERRET.getKey(), new CACWorldGen.SpawnValues(4, 2, 3));
+        registerSpawns(builder, CACEntities.FERRET.getKey(), new CACWorldGen.SpawnValues(4, 2, 3));
+        registerSpawns(builder, CACEntities.OTTER.getKey(), new CACWorldGen.SpawnValues(1, 3, 5));
+        registerSpawns(builder, CACEntities.KOI_FISH.getKey(), new CACWorldGen.SpawnValues(4, 2, 5));
+        registerSpawns(builder, CACEntities.DRAGONFLY.getKey(), new CACWorldGen.SpawnValues(7, 1, 1));
+        registerSpawns(builder, CACEntities.SHIMA_ENAGA.getKey(), new CACWorldGen.SpawnValues(3, 2, 3));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(32, 1, 4));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(8, 1, 1));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(16, 1, 4));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(6, 1, 1));
+        registerSpawns(builder, CACEntities.SEA_BUNNY.getKey(), new CACWorldGen.SpawnValues(16, 1, 4));
+        registerSpawns(builder, CACEntities.DUMBO_OCTOPUS.getKey(), new CACWorldGen.SpawnValues(6, 1, 1));
+
+        builder.pop();
+
+        CACWorldGen.registerSpawns(this);
+    }
+
+}

forge/src/main/java/com/github/eterdelta/crittersandcompanions/CrittersAndCompanionsForge.java
@@ -4,6 +4,8 @@ import static com.github.eterdelta.crittersandcompanions.CrittersAndCompanions.M
 
 import com.github.eterdelta.crittersandcompanions.handler.PlayerHandler;
 import java.nio.file.Path;
+
+import com.github.eterdelta.crittersandcompanions.platform.ForgeConfigs;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.repository.Pack;
@@ -16,6 +18,7 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -30,6 +33,7 @@ import net.minecraftforge.resource.PathPackResources;
 public class CrittersAndCompanionsForge {
 
     public CrittersAndCompanionsForge() {
+        ForgeConfigs.register(ModLoadingContext.get());
         CrittersAndCompanions.init();
 
         var modBus = FMLJavaModLoadingContext.get().getModEventBus();