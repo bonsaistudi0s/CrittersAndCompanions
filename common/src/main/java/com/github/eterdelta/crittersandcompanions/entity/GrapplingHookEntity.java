a9ddb57544c1aa33e9f4baced1cbdc982b5a382e
domi <domigc@outlook.cl>
2025-12-25 20:28:57 -0300

    Fix: Prevent crash by adding null check to GrapplingHookEntity

--- common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/GrapplingHookEntity.java
+++ common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/GrapplingHookEntity.java
@@ -47,6 +47,10 @@ public class GrapplingHookEntity extends ThrowableItemProjectile {
             addedToWorld = true;
         }
 
+        if (getOwner() == null) {
+            discard();
+            return;
+        }
         var offsetLengthSqr = distanceToSqr(getOwner());
 
         var maxDistance = Services.CONFIGS.common().grapplingHookMaxDistance.get();
