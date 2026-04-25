11f3fd29fac89d065625f09b777182af59f8e090
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-04-26 00:49:32 +0200

    refactor: keep insertion order of behaviours

--- common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/brain/behaviour/Behaviours.java
+++ common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/brain/behaviour/Behaviours.java
@@ -1,13 +1,13 @@
 package com.github.eterdelta.crittersandcompanions.entity.brain.behaviour;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
 
 public class Behaviours {
 
-    private final Map<Class<? extends Behaviour>, Behaviour> entries = new HashMap<>();
+    private final Map<Class<? extends Behaviour>, Behaviour> entries = new LinkedHashMap<>();
 
     public Behaviours add(Behaviour behaviour) {
         if (this.entries.put(behaviour.getClass(), behaviour) != null) {
@@ -26,12 +26,10 @@ public class Behaviours {
 
     @SuppressWarnings("unchecked")
     public <T extends Behaviour> Optional<T> optional(Class<T> type) {
-        return Optional.ofNullable(entries.get(type))
-                .map(it -> (T) it);
+        return Optional.ofNullable(entries.get(type)).map(it -> (T) it);
     }
 
     public <T extends Behaviour> T the(Class<T> type) {
         return optional(type).orElseThrow();
     }
-
 }
