b29cdf7d44092eb573efdb1d36888ca5c091fa4a
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-06-24 18:55:30 +0200

    feat(weevil): add mud ball ranged attack against enemies

--- common/src/main/java/com/github/eterdelta/crittersandcompanions/registry/CACEntities.java
+++ common/src/main/java/com/github/eterdelta/crittersandcompanions/registry/CACEntities.java
@@ -18,6 +18,7 @@ import com.github.eterdelta.crittersandcompanions.entity.SnailEntity;
 import com.github.eterdelta.crittersandcompanions.entity.StagBeetleEntity;
 import com.github.eterdelta.crittersandcompanions.entity.StickBugEntity;
 import com.github.eterdelta.crittersandcompanions.entity.WeevilEntity;
+import com.github.eterdelta.crittersandcompanions.entity.projectiles.MudBallProjectile;
 import com.github.eterdelta.crittersandcompanions.platform.RegistryEntry;
 import com.github.eterdelta.crittersandcompanions.platform.RegistryHelper;
 import com.github.eterdelta.crittersandcompanions.platform.Services;
@@ -33,7 +34,6 @@ public class CACEntities {
     public static final RegistryEntry<EntityType<DragonflyEntity>> DRAGONFLY = register("dragonfly", () -> EntityType.Builder.of(DragonflyEntity::new, MobCategory.AMBIENT).sized(0.9F, 0.4F));
     public static final RegistryEntry<EntityType<DumboOctopusEntity>> DUMBO_OCTOPUS = register("dumbo_octopus", () -> EntityType.Builder.of(DumboOctopusEntity::new, MobCategory.WATER_AMBIENT).sized(0.4F, 0.4F));
     public static final RegistryEntry<EntityType<FerretEntity>> FERRET = register("ferret", () -> EntityType.Builder.of(FerretEntity::new, MobCategory.CREATURE).sized(0.8F, 0.7F));
-    public static final RegistryEntry<EntityType<GrapplingHookEntity>> GRAPPLING_HOOK = register("grappling_hook", () -> EntityType.Builder.<GrapplingHookEntity>of(GrapplingHookEntity::new, MobCategory.MISC).sized(0.2F, 0.2F).noSave().noSummon());
     public static final RegistryEntry<EntityType<JumpingSpiderEntity>> JUMPING_SPIDER = register("jumping_spider", () -> EntityType.Builder.of(JumpingSpiderEntity::new, MobCategory.CREATURE).sized(0.5F, 0.4F));
     public static final RegistryEntry<EntityType<KoiFishEntity>> KOI_FISH = register("koi_fish", () -> EntityType.Builder.of(KoiFishEntity::new, MobCategory.WATER_AMBIENT).sized(0.6F, 0.3F));
     public static final RegistryEntry<EntityType<LeafInsectEntity>> LEAF_INSECT = register("leaf_insect", () -> EntityType.Builder.of(LeafInsectEntity::new, MobCategory.AMBIENT).sized(0.4F, 0.3F));
@@ -48,6 +48,9 @@ public class CACEntities {
     public static final RegistryEntry<EntityType<SnailEntity>> SNAIL = register("snail", () -> EntityType.Builder.of(SnailEntity::new, MobCategory.AMBIENT).sized(0.5F, 0.575F));
     public static final RegistryEntry<EntityType<StickBugEntity>> STICK_BUG = register("stick_bug", () -> EntityType.Builder.of(StickBugEntity::new, MobCategory.AMBIENT).sized(0.5F, 0.385F));
     public static final RegistryEntry<EntityType<WeevilEntity>> WEEVIL = register("weevil", () -> EntityType.Builder.of(WeevilEntity::new, MobCategory.AMBIENT).sized(0.5F, 0.5F));
+
+    public static final RegistryEntry<EntityType<GrapplingHookEntity>> GRAPPLING_HOOK = register("grappling_hook", () -> EntityType.Builder.<GrapplingHookEntity>of(GrapplingHookEntity::new, MobCategory.MISC).sized(0.2F, 0.2F).noSave().noSummon());
+    public static final RegistryEntry<EntityType<MudBallProjectile>> MUD_BALL = register("mud_ball", () -> EntityType.Builder.<MudBallProjectile>of(MudBallProjectile::new, MobCategory.MISC).sized(0.25F, 0.25F).clientTrackingRange(4).updateInterval(10));
 
     private static <T extends Entity> RegistryEntry<EntityType<T>> register(String name, Supplier<EntityType.Builder<T>> factory) {
         return ENTITIES.register(name, () -> factory.get().build(name));
