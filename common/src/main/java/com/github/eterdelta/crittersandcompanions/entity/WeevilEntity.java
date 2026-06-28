ce0b607f6237e018bd076693dba1db3fd2a29953
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-06-28 20:43:35 +0200

    refactor: extract ai goal to own class

--- common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/WeevilEntity.java
+++ common/src/main/java/com/github/eterdelta/crittersandcompanions/entity/WeevilEntity.java
@@ -5,6 +5,7 @@ import com.github.eterdelta.crittersandcompanions.entity.brain.behaviour.Behavio
 import com.github.eterdelta.crittersandcompanions.entity.brain.behaviour.DancingBehaviour;
 import com.github.eterdelta.crittersandcompanions.entity.brain.behaviour.HealthRegenerationBehaviour;
 import com.github.eterdelta.crittersandcompanions.entity.brain.behaviour.TameableBehaviour;
+import com.github.eterdelta.crittersandcompanions.entity.brain.goal.AnimatedDelayedRangedAttackGoal;
 import com.github.eterdelta.crittersandcompanions.entity.brain.goal.DancingStrollGoal;
 import com.github.eterdelta.crittersandcompanions.entity.brain.goal.TameablePanicGoal;
 import com.github.eterdelta.crittersandcompanions.entity.projectiles.MudBallProjectile;
@@ -45,7 +46,7 @@ public class WeevilEntity extends TamableAnimal implements GeoEntity, RangedAtta
 
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
-    private final DelayedRangedAttackGoal rangedAttackGoal = new DelayedRangedAttackGoal(this, 1, 20, 8, "controller", "throw", 5);
+    private final AnimatedDelayedRangedAttackGoal<WeevilEntity> rangedAttackGoal = new AnimatedDelayedRangedAttackGoal<>(this, 1, 20, 8, "controller", "throw", 5);
     private final HurtByTargetGoal hurtByTargetGoal = new HurtByTargetGoal(this);
     private final NearestAttackableTargetGoal<Mob> attackTargetGoal = new NearestAttackableTargetGoal<>(this, Mob.class, 5, true, false, this::shouldAttack);
 
@@ -160,117 +161,5 @@ public class WeevilEntity extends TamableAnimal implements GeoEntity, RangedAtta
         }
 
         return super.canAttack(target);
-    }
-
-    private static class DelayedRangedAttackGoal extends Goal {
-        private final WeevilEntity owner;
-        private @Nullable LivingEntity target;
-        private int attackTime = -1;
-        private final double speedModifier;
-        private int seeTime;
-        private final int attackIntervalMin;
-        private final int attackIntervalMax;
-        private final float attackRadius;
-        private final float attackRadiusSqr;
-
-        private final String animationControllerName;
-        private final String animationName;
-        private int ticksUntilAttack;
-        private final int windUpTicks;
-        private boolean isWindingUp;
-
-        public DelayedRangedAttackGoal(final WeevilEntity owner, final double speedModifier, final int attackInterval, final float attackRadius, final String animationControllerName, final String animationName, final int windUpTicks) {
-            this(owner, speedModifier, attackInterval, attackInterval, attackRadius, animationControllerName, animationName, windUpTicks);
-        }
-
-        public DelayedRangedAttackGoal(final WeevilEntity owner, final double speedModifier, final int attackIntervalMin, final int attackIntervalMax, final float attackRadius, final String animationControllerName, final String animationName, final int windUpTicks) {
-            this.owner = owner;
-            this.speedModifier = speedModifier;
-            this.attackIntervalMin = attackIntervalMin;
-            this.attackIntervalMax = attackIntervalMax;
-            this.attackRadius = attackRadius;
-            this.attackRadiusSqr = attackRadius * attackRadius;
-            this.animationControllerName = animationControllerName;
-            this.animationName = animationName;
-            this.windUpTicks = windUpTicks;
-            this.setFlags(EnumSet.of(Flag.MOVE, Flag.LOOK));
-        }
-
-        @Override
-        public boolean canUse() {
-            var bestTarget = this.owner.getTarget();
-            if (bestTarget != null && bestTarget.isAlive()) {
-                this.target = bestTarget;
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public boolean canContinueToUse() {
-            return this.canUse() || this.target.isAlive() && !this.owner.getNavigation().isDone();
-        }
-
-        @Override
-        public void stop() {
-            this.target = null;
-            this.seeTime = 0;
-            this.attackTime = -1;
-            isWindingUp = false;
-        }
-
-        @Override
-        public boolean requiresUpdateEveryTick() {
-            return true;
-        }
-
-        @Override
-        public void tick() {
-            double targetDistSqr = this.owner.distanceToSqr(this.target.getX(), this.target.getY(), this.target.getZ());
-            var hasLineOfSight = this.owner.getSensing().hasLineOfSight(this.target);
-            if (hasLineOfSight) {
-                this.seeTime++;
-            } else {
-                this.seeTime = 0;
-            }
-
-            this.owner.getLookControl().setLookAt(this.target, 30.0F, 30.0F);
-
-            if (isWindingUp) {
-                this.owner.getNavigation().stop();
-
-                ticksUntilAttack--;
-
-                if (ticksUntilAttack <= 0) {
-                    float dist = (float)Math.sqrt(targetDistSqr) / this.attackRadius;
-                    float power = Mth.clamp(dist, 0.1F, 1.0F);
-                    this.owner.performRangedAttack(this.target, power);
-                    this.attackTime = Mth.floor(dist * (this.attackIntervalMax - this.attackIntervalMin) + this.attackIntervalMin);
-                    isWindingUp = false;
-                }
-
-                return;
-            }
-
-            if (!(targetDistSqr > this.attackRadiusSqr) && this.seeTime >= 5) {
-                this.owner.getNavigation().stop();
-            } else {
-                this.owner.getNavigation().moveTo(this.target, this.speedModifier);
-            }
-
-            if (--this.attackTime == 0) {
-                if (!hasLineOfSight) {
-                    return;
-                }
-
-                isWindingUp = true;
-                ticksUntilAttack = windUpTicks;
-
-                owner.triggerAnim(animationControllerName, animationName);
-            } else if (this.attackTime < 0) {
-                this.attackTime = Mth.floor(Mth.lerp(Math.sqrt(targetDistSqr) / this.attackRadius, this.attackIntervalMin, this.attackIntervalMax));
-            }
-        }
     }
 }
