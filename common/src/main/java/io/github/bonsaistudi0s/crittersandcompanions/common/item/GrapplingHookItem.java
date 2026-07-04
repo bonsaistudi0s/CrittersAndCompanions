98536d8b121fd202f8d56255d4ac54aa7ac9fef7
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-07-04 20:39:18 +0200

    tweak: add durability to grappling hook with silk as repair material

--- common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/item/GrapplingHookItem.java
+++ common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/item/GrapplingHookItem.java
@@ -4,14 +4,18 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 
+import org.jetbrains.annotations.NotNull;
+
 import io.github.bonsaistudi0s.crittersandcompanions.common.entity.GrapplingHookEntity;
 import io.github.bonsaistudi0s.crittersandcompanions.common.extension.IGrapplingState;
+import io.github.bonsaistudi0s.crittersandcompanions.common.registry.CACItems;
 
 public class GrapplingHookItem extends Item {
     public GrapplingHookItem(Properties properties) {
@@ -19,13 +23,14 @@ public class GrapplingHookItem extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+    public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
         ItemStack stack = player.getItemInHand(interactionHand);
         var grapplingState = (IGrapplingState) player;
 
         if (grapplingState.getHook() != null) {
             grapplingState.getHook().pull();
             level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.NEUTRAL, 0.25F, 1.0F + 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
+            stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(interactionHand));
             player.gameEvent(GameEvent.ITEM_INTERACT_FINISH);
         } else {
             if (!level.isClientSide()) {
@@ -38,5 +43,10 @@ public class GrapplingHookItem extends Item {
         }
 
         return InteractionResultHolder.sidedSuccess(stack, level.isClientSide());
+    }
+
+    @Override
+    public boolean isValidRepairItem(ItemStack stack, ItemStack repairCandidate) {
+        return repairCandidate.is(CACItems.SILK) || super.isValidRepairItem(stack, repairCandidate);
     }
 }
--- common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/registry/CACItems.java
+++ common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/registry/CACItems.java
@@ -34,7 +34,7 @@ public class CACItems {
     public static final RegistrySupplier<Item> SEA_BUNNY_SLIME_BLOCK = ITEMS.register("sea_bunny_slime_block", () -> new BlockItem(CACBlocks.SEA_BUNNY_SLIME_BLOCK.get(), baseProperties()));
 
     public static final RegistrySupplier<Item> SILK_LEAD = ITEMS.register("silk_lead", () -> new SilkLeashItem(baseProperties()));
-    public static final RegistrySupplier<Item> GRAPPLING_HOOK = ITEMS.register("grappling_hook", () -> new GrapplingHookItem(baseProperties().stacksTo(1)));
+    public static final RegistrySupplier<Item> GRAPPLING_HOOK = ITEMS.register("grappling_hook", () -> new GrapplingHookItem(baseProperties().stacksTo(1).durability(128)));
 
     public static final RegistrySupplier<Item> PEARL_NECKLACE_1 = ITEMS.register("pearl_necklace_1", () -> new PearlNecklaceItem(baseProperties().stacksTo(1), 1));
     public static final RegistrySupplier<Item> PEARL_NECKLACE_2 = ITEMS.register("pearl_necklace_2", () -> new PearlNecklaceItem(baseProperties().stacksTo(1), 2));
