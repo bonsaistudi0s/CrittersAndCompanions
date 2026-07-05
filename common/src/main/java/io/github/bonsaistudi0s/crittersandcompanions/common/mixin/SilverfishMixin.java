215bea8d7b41a001cad31afe2c40ccc01e9bdbf2
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-07-05 17:25:22 +0200

    feat: make silverfish scared of jumping spiders

---
+++ common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/mixin/SilverfishMixin.java
@@ -0,0 +1,37 @@
+package io.github.bonsaistudi0s.crittersandcompanions.common.mixin;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.Silverfish;
+import net.minecraft.world.level.Level;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import io.github.bonsaistudi0s.crittersandcompanions.common.entity.JumpingSpiderEntity;
+
+@Mixin(Silverfish.class)
+public class SilverfishMixin extends Monster {
+
+    protected SilverfishMixin(EntityType<? extends Monster> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(
+            method = "registerGoals",
+            at = @At("TAIL")
+    )
+    private void cac$addFleeFromJumpingSpider(CallbackInfo ci) {
+        var self = (Silverfish) (Object) this;
+        this.goalSelector.addGoal(2, new AvoidEntityGoal<>(
+                self,
+                JumpingSpiderEntity.class,
+                8.0F,
+                1.0D,
+                1.2D
+        ));
+    }
+}
