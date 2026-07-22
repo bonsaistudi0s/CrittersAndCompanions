c418c44a2444975dd42aed62e86ae000242fc9a8
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-07-22 20:52:32 +0200

    fix: prevent crash on dedicated servers

--- common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/network/CACPacketHandler.java
+++ common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/network/CACPacketHandler.java
@@ -1,5 +1,7 @@
 package io.github.bonsaistudi0s.crittersandcompanions.common.network;
 
+import dev.architectury.platform.Platform;
+import net.fabricmc.api.EnvType;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerPlayer;
@@ -14,9 +16,19 @@ import dev.architectury.networking.NetworkManager;
 public class CACPacketHandler {
 
     public static void registerPackets() {
-        NetworkManager.registerReceiver(NetworkManager.Side.S2C, ClientboundBubbleStatePacket.TYPE.type(), ClientboundBubbleStatePacket.TYPE.codec(), ClientboundBubbleStatePacket::handle);
-        NetworkManager.registerReceiver(NetworkManager.Side.S2C, ClientboundGrapplingStatePacket.TYPE.type(), ClientboundGrapplingStatePacket.TYPE.codec(), ClientboundGrapplingStatePacket::handle);
-        NetworkManager.registerReceiver(NetworkManager.Side.S2C, ClientboundSilkLeashStatePacket.TYPE.type(), ClientboundSilkLeashStatePacket.TYPE.codec(), ClientboundSilkLeashStatePacket::handle);
+        // https://github.com/architectury/architectury-api/issues/680
+        // S2C packets need to be split up so the dedicated server doesn't try to register a client handler (on fabric at least)
+        if (Platform.getEnv() == EnvType.CLIENT) {
+            NetworkManager.registerReceiver(NetworkManager.Side.S2C, ClientboundBubbleStatePacket.TYPE.type(), ClientboundBubbleStatePacket.TYPE.codec(), ClientboundBubbleStatePacket::handle);
+            NetworkManager.registerReceiver(NetworkManager.Side.S2C, ClientboundGrapplingStatePacket.TYPE.type(), ClientboundGrapplingStatePacket.TYPE.codec(), ClientboundGrapplingStatePacket::handle);
+            NetworkManager.registerReceiver(NetworkManager.Side.S2C, ClientboundSilkLeashStatePacket.TYPE.type(), ClientboundSilkLeashStatePacket.TYPE.codec(), ClientboundSilkLeashStatePacket::handle);
+        } else {
+            NetworkManager.registerS2CPayloadType(ClientboundBubbleStatePacket.TYPE.type(), ClientboundBubbleStatePacket.TYPE.codec());
+            NetworkManager.registerS2CPayloadType(ClientboundGrapplingStatePacket.TYPE.type(), ClientboundGrapplingStatePacket.TYPE.codec());
+            NetworkManager.registerS2CPayloadType(ClientboundSilkLeashStatePacket.TYPE.type(), ClientboundSilkLeashStatePacket.TYPE.codec());
+        }
+
+        // C2S would go here (if the issue is not resolved yet)
     }
 
     public static <T extends CustomPacketPayload> void sendToPlayer(ServerPlayer player, T payload) {
