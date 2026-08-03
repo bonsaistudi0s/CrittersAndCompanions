6315409288e9af1652393a9ff4edd821daf1a512
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-08-03 20:03:46 +0200

    tweak(jumping-spider): change mob category to ambient

--- common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/entity/JumpingSpiderEntity.java
+++ common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/entity/JumpingSpiderEntity.java
@@ -194,4 +194,9 @@ public class JumpingSpiderEntity extends TamableAnimal implements GeoEntity {
 
         return super.wantsToAttack(target, owner);
     }
+
+    @Override
+    public boolean removeWhenFarAway(double distanceToClosestPlayer) {
+        return !isTame() && !hasCustomName();
+    }
 }
