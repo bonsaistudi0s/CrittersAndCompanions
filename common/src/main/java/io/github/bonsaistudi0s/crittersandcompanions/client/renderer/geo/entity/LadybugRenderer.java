a6216e3ea51a3a307e74369c7d32cc48963d093c
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-08-04 23:11:59 +0200

    feat: add new baby variants

--- common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/client/renderer/geo/entity/LadybugRenderer.java
+++ common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/client/renderer/geo/entity/LadybugRenderer.java
@@ -1,14 +1,14 @@
 package io.github.bonsaistudi0s.crittersandcompanions.client.renderer.geo.entity;
 
 import io.github.bonsaistudi0s.crittersandcompanions.CrittersAndCompanions;
-import io.github.bonsaistudi0s.crittersandcompanions.client.renderer.geo.entity.base.AgingScaledGeoEntityRenderer;
+import io.github.bonsaistudi0s.crittersandcompanions.client.model.geo.AgingGeoModel;
 import io.github.bonsaistudi0s.crittersandcompanions.common.entity.LadybugEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import software.bernie.geckolib.model.DefaultedEntityGeoModel;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
-public class LadybugRenderer extends AgingScaledGeoEntityRenderer<LadybugEntity> {
+public class LadybugRenderer extends GeoEntityRenderer<LadybugEntity> {
 
     public LadybugRenderer(EntityRendererProvider.Context renderManager) {
-        super(renderManager, new DefaultedEntityGeoModel<>(CrittersAndCompanions.createId("ladybug")));
+        super(renderManager, new AgingGeoModel<>(CrittersAndCompanions.createId("ladybug")));
     }
 }
