90a1074e8a451b7b1f63094dfaa2b9434595cd76
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-08-04 23:12:00 +0200

    fix(dumbo-octopus): resolve bubble not being visible

--- forge/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/forge/client/CrittersAndCompanionsForgeClient.java
+++ forge/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/forge/client/CrittersAndCompanionsForgeClient.java
@@ -1,8 +1,14 @@
 package io.github.bonsaistudi0s.crittersandcompanions.forge.client;
 
 import io.github.bonsaistudi0s.crittersandcompanions.client.CrittersAndCompanionsClient;
+import io.github.bonsaistudi0s.crittersandcompanions.client.renderer.BubbleLayer;
 import io.github.bonsaistudi0s.crittersandcompanions.common.config.CACCommonConfig;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -12,6 +18,7 @@ public class CrittersAndCompanionsForgeClient {
     public static void init(IEventBus modEventBus) {
         CrittersAndCompanionsClient.init();
         modEventBus.addListener(CrittersAndCompanionsForgeClient::onClientSetup);
+        modEventBus.addListener(CrittersAndCompanionsForgeClient::addLayers);
 
         //noinspection removal
         ModLoadingContext.get().registerExtensionPoint(
@@ -20,6 +27,17 @@ public class CrittersAndCompanionsForgeClient {
                         (client, parent) -> CACCommonConfig.createConfigScreen(parent)
                 )
         );
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private static void addLayers(EntityRenderersEvent.AddLayers event) {
+        for (var skin : event.getSkins()) {
+            var renderer = event.getSkin(skin);
+            if (renderer != null) {
+                var playerRenderer = (RenderLayerParent<Player, PlayerModel<Player>>) renderer;
+                ((LivingEntityRenderer) renderer).addLayer(new BubbleLayer(playerRenderer, event.getEntityModels()));
+            }
+        }
     }
 
     private static void onClientSetup(FMLClientSetupEvent event) {
