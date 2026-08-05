7237c24a8b44655b388c548255d12e8a0f6393ea
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-08-05 22:26:04 +0200

    feat: add recipe to turn snail slime bottles into slime balls

--- forge/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/datagen/provider/CACRecipes.java
+++ forge/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/datagen/provider/CACRecipes.java
@@ -139,5 +139,10 @@ public final class CACRecipes extends RecipeProvider {
                 .requires(CACItems.CLAM.get())
                 .unlockedBy("has_clam", has(CACItems.CLAM.get()))
                 .save(consumer, CrittersAndCompanions.createId("bone_meal_from_clam"));
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, Items.SLIME_BALL)
+                .requires(CACItems.SNAIL_SLIME_BOTTLE.get())
+                .unlockedBy("has_snail_slime_bottle", has(CACItems.SNAIL_SLIME_BOTTLE.get()))
+                .save(consumer, CrittersAndCompanions.createId("slime_ball_from_snail_slime_bottle"));
     }
 }
