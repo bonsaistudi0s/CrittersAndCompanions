f0743d522dd97b1e8f3b98e816c9d6ea133a88b4
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-08-06 16:32:00 +0200

    fix(red-panda): resolve scaring animation glitch

--- common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/entity/RedPandaEntity.java
+++ common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/entity/RedPandaEntity.java
@@ -29,8 +29,10 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animation.*;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.AnimationController;
 import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.animation.RawAnimation;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
@@ -59,7 +61,7 @@ public class RedPandaEntity extends TamableAnimal implements GeoEntity {
     private static final EntityDataAccessor<Boolean> ALERT = SynchedEntityData.defineId(RedPandaEntity.class, EntityDataSerializers.BOOLEAN);
 
     private static final int TRANSITION_TICK_TIME = 3;
-    private static final RawAnimation ANGRY_ANIM = RawAnimation.begin().then("angry", Animation.LoopType.PLAY_ONCE);
+    private static final RawAnimation ANGRY_ANIM = RawAnimation.begin().thenPlay("angry");
     private static final RawAnimation SIT_ANIM = RawAnimation.begin().thenLoop("sit");
     private static final RawAnimation SLEEP_ANIM = RawAnimation.begin().thenLoop("sleep");
     private static final RawAnimation SWIM_ANIM = RawAnimation.begin().thenLoop("swim");
@@ -299,7 +301,7 @@ public class RedPandaEntity extends TamableAnimal implements GeoEntity {
 
         public AlertGoal() {
             this.setFlags(EnumSet.of(Flag.MOVE, Flag.LOOK));
-            this.time = reducedTickDelay(40);
+            this.time = reducedTickDelay(25);
         }
 
         @Override
@@ -324,6 +326,7 @@ public class RedPandaEntity extends TamableAnimal implements GeoEntity {
 
         @Override
         public void start() {
+            this.time = reducedTickDelay(25);
             RedPandaEntity.this.setAlert(true);
             RedPandaEntity.this.getNavigation().stop();
             RedPandaEntity.this.getMoveControl().setWantedPosition(RedPandaEntity.this.getX(), RedPandaEntity.this.getY(), RedPandaEntity.this.getZ(), 0.0D);
@@ -337,7 +340,7 @@ public class RedPandaEntity extends TamableAnimal implements GeoEntity {
 
         @Override
         public void stop() {
-            this.time = 20;
+            this.time = 25;
             RedPandaEntity.this.setAlert(false);
         }
     }
