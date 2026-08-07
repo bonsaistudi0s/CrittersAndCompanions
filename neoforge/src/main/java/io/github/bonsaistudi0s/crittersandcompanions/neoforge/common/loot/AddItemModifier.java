de224512a6fe0b2baee82124a359ad733c5649f0
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-08-07 02:43:27 +0200

    fix: resolve potential issue with custom loot modifiers

--- neoforge/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/neoforge/common/loot/AddItemModifier.java
+++ neoforge/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/neoforge/common/loot/AddItemModifier.java
@@ -30,7 +30,7 @@ public class AddItemModifier extends LootModifier {
 
     @Override
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> drops, LootContext context) {
-        drops.add(item);
+        drops.add(item.copy());
         return drops;
     }
 
