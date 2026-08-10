e50b46df4cc9f5e689c03cf0d68b100b4b831354
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-08-10 21:07:24 +0200

    tweak(weevil): rename to "Acorn Weevil"

--- neoforge/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/datagen/provider/CACLang.java
+++ neoforge/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/datagen/provider/CACLang.java
@@ -39,11 +39,10 @@ public final class CACLang extends LanguageProvider {
         withSpawnEgg(CACEntities.LEAF_INSECT);
         withSpawnEgg(CACEntities.LADYBUG);
         withSpawnEgg(CACEntities.STAG_BEETLE);
-        add(CACEntities.ROLY_POLY.get(), "Roly-Poly");
-        addSpawnEgg(CACEntities.ROLY_POLY, "Roly-Poly");
+        withSpawnEggAndCustomTranslation(CACEntities.ROLY_POLY, "Roly-Poly");
         withSpawnEgg(CACEntities.SNAIL);
         withSpawnEgg(CACEntities.STICK_BUG);
-        withSpawnEgg(CACEntities.WEEVIL);
+        withSpawnEggAndCustomTranslation(CACEntities.WEEVIL, "Acorn Weevil");
         add(CACEntities.GRAPPLING_HOOK.get(), "Grappling Hook");
 
         translate(CACBlocks.SILK_COCOON.getKey());
@@ -135,6 +134,11 @@ public final class CACLang extends LanguageProvider {
         var translation = defaultTranslation(id.getPath());
         add(type.get(), translation);
         add("item.%s.%s_spawn_egg".formatted(id.getNamespace(), id.getPath()), translation + " Spawn Egg");
+    }
+
+    private void withSpawnEggAndCustomTranslation(RegistrySupplier<? extends EntityType<?>> type, String customTranslation) {
+        add(type.get(), customTranslation);
+        addSpawnEgg(type, customTranslation);
     }
 
     private void addSpawnEgg(RegistrySupplier<? extends EntityType<?>> type, String translation) {
