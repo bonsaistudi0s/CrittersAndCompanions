7f7ace92fa904f6d17a13dc21e2e62d5ad730c0a
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2026-08-11 21:24:37 +0200

    tweak(jumping-spider): improve leaping mechanics

--- common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/entity/JumpingSpiderEntity.java
+++ common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/entity/JumpingSpiderEntity.java
@@ -3,6 +3,7 @@ package io.github.bonsaistudi0s.crittersandcompanions.common.entity;
 import io.github.bonsaistudi0s.crittersandcompanions.common.entity.brain.behaviour.*;
 import io.github.bonsaistudi0s.crittersandcompanions.common.entity.brain.control.JumpingSpiderMoveControl;
 import io.github.bonsaistudi0s.crittersandcompanions.common.entity.brain.goal.DancingStrollGoal;
+import io.github.bonsaistudi0s.crittersandcompanions.common.entity.brain.goal.JumpingSpiderLeapGoal;
 import io.github.bonsaistudi0s.crittersandcompanions.common.entity.brain.goal.TamableAnimalPanicGoal;
 import io.github.bonsaistudi0s.crittersandcompanions.common.registry.AnimalTags;
 import io.github.bonsaistudi0s.crittersandcompanions.common.registry.CACEntities;
@@ -83,7 +84,7 @@ public class JumpingSpiderEntity extends TamableAnimal implements GeoEntity {
         this.goalSelector.addGoal(2, new SitWhenOrderedToGoal(this));
         this.goalSelector.addGoal(3, new BreedGoal(this, 1.0D));
         this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.25D));
-        this.goalSelector.addGoal(5, new LeapAtTargetGoal(this, 0.4F));
+        this.goalSelector.addGoal(5, new JumpingSpiderLeapGoal(this, 0.4F, 0.8F));
         this.goalSelector.addGoal(6, new MeleeAttackGoal(this, 1.0D, true));
         this.goalSelector.addGoal(7, TAGS.temptGoal(this));
         this.goalSelector.addGoal(8, new FollowOwnerGoal(this, 1.0D, 5.0F, 1.0F, false));
--- common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/entity/brain/control/JumpingSpiderMoveControl.java
+++ common/src/main/java/io/github/bonsaistudi0s/crittersandcompanions/common/entity/brain/control/JumpingSpiderMoveControl.java
@@ -2,8 +2,10 @@ package io.github.bonsaistudi0s.crittersandcompanions.common.entity.brain.contro
 
 import io.github.bonsaistudi0s.crittersandcompanions.common.entity.JumpingSpiderEntity;
 import net.minecraft.world.entity.ai.control.MoveControl;
+import net.minecraft.world.phys.Vec3;
 
 public class JumpingSpiderMoveControl extends MoveControl {
+
     private final JumpingSpiderEntity spider;
 
     public JumpingSpiderMoveControl(JumpingSpiderEntity jumpingSpider) {
@@ -12,9 +14,18 @@ public class JumpingSpiderMoveControl extends MoveControl {
     }
 
     public void tick() {
-        if (this.hasWanted() && this.spider.onGround() && this.spider.getRandom().nextFloat() <= 0.05F) {
-            this.spider.setDeltaMovement(this.spider.getDeltaMovement().add(0.0D, 0.6D, 0.0D));
+        if (this.hasWanted() && this.spider.onGround() && this.spider.getRandom().nextFloat() <= 0.025F) {
+            var dx = this.wantedX - this.spider.getX();
+            var dz = this.wantedZ - this.spider.getZ();
+            var jumpDir = new Vec3(dx, 0.0, dz).normalize();
+            this.spider.setDeltaMovement(
+                    this.spider.getDeltaMovement().add(
+                            jumpDir.x, 0.5, jumpDir.z
+                    )
+            );
+            this.spider.getNavigation().stop();
         }
+
         super.tick();
     }
 }
